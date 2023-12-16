930fb0672f767be3d95d79aecfa500077c38f26c
David3D-AndweM <d.mwape@alustudent.com>
2023-12-15 17:48:17 -0800

    commit

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,11 @@
-#increase the ULIMIT
+#nginx failed requests
+
 exec { 'fix--for-nginx':
-#modify the ULIMIT value
-    command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-    #specific
-    path    => '/usr/local/bin/:/bin/',
+    command => 'sed -i "s/15/4096/" /etc/default/nginx',
+    path    => '/usr/local/bin/:/bin/'
 }
 
-restart
-exec { 'nginx-restart':
-    #restart Nginx
-    command => '/etc/init.d/nginx restart'
-    #specific 
-    path    => '/etc/init.d'/,
+-> exec { 'nginx-restart':
+    command => 'nginx restart',
+    path    => '/etc/init.d/'
 }
